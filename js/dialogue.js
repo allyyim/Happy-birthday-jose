c2d0e43d4143d504fcf86c188d71d66b62f92690
Alison Yim <67910076+allyyim@users.noreply.github.com>
2023-04-02 21:12:35 -0400

    Update dialogue.js

--- js/dialogue.js
+++ js/dialogue.js
@@ -115,7 +115,7 @@ var dialogue = [
     },
     {
         id: "credits",
-        text: "Programming & Bad Jokes<br>Alison Yimbr><br>Music<br>Dani Sweet<br><br>Star / Birthday Boy<br>Cole Smith<br><br>My 11:11 Wish",
+        text: "Programming & Bad Jokes<br>Alison Yim<br><br>Music<br>Dani Sweet<br><br>Star / Birthday Boy<br>Cole Smith<br><br>My 11:11 Wish",
         next:"credits2",
         activate:function(){exitDialogue();}
     },
